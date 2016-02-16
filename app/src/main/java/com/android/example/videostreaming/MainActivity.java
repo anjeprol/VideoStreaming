d81b48b963d35496a6d109d5784d302671afd9c5
anjeprol <anjeprol_prado@hotmail.com>
2016-02-16 11:36:21 -0500

    Fisrt Version

---
+++ app/src/main/java/com/android/example/videostreaming/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.example.videostreaming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.myVideo);
+        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        videoView.setVideoURI(vidUri);
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(videoView);
+        videoView.setMediaController(vidControl);
+        videoView.start();
+    }
+}
